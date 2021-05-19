8508f5f8919bfda10420595f2a776657af2d1a76
tiagobarros01 <tbarros1337@hotmail.com>
2021-05-19 16:11:13 -0300

    style: styling a HomeStyles

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -6,10 +6,10 @@ import { HomeStyles } from '../styles/Home';
 export default function Home() {
   return (
     <SafeAreaView style={HomeStyles.Area}>
-      <View>
-        <Text>ONEBITHEALTH</Text>
+      <View style={HomeStyles.Title}>
+        <Text>ONE BIT HEALTH</Text>
       </View>
-      <View>
+      <View style={HomeStyles.Main}>
         <InputTextInfo PlaceholderValue="Ex. 1.75" TextValue="Height" />
         <InputTextInfo PlaceholderValue="Ex. 70.55" TextValue="Weight" />
       </View>
--- src/styles/Home.ts
+++ src/styles/Home.ts
@@ -3,8 +3,23 @@ import { StyleSheet } from "react-native";
 export const HomeStyles = StyleSheet.create({
   Area: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: '#eee',
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  Title: {
+    height: 150,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  Main: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+
+    backgroundColor: '#fff',
+
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
   }
 })
