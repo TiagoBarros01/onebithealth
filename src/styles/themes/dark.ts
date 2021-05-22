a17357bea41e227c9b69a434ee37362ed06c0523
tiagobarros01 <tbarros1337@hotmail.com>
2021-05-21 23:50:07 -0300

    style: Addes a new Theme prop

--- src/styles/themes/dark.ts
+++ src/styles/themes/dark.ts
@@ -5,6 +5,7 @@ interface ThemeProps {
     background: string;
     textColor: string;
     inputColor: string;
+    lightBackground: string;
     red: string;
     white: string;
   };
@@ -12,12 +13,14 @@ interface ThemeProps {
 }
 
 export const Theme: ThemeProps = {
-  title: 'dark',
+  title: 'light',
 
   colors: {
     background: '#e0e5e5',
     textColor: '#222',
     inputColor: '#f6f6f6',
+
+    lightBackground: '#e9e9e9',
   
     red: '#ff0043',
     white: '#fff',
--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -5,6 +5,7 @@ interface ThemeProps {
     background: string;
     textColor: string;
     inputColor: string;
+    lightBackground: string;
     red: string;
     white: string;
   };
@@ -18,6 +19,8 @@ export const Theme: ThemeProps = {
     background: '#e0e5e5',
     textColor: '#222',
     inputColor: '#f6f6f6',
+
+    lightBackground: '#e9e9e9',
   
     red: '#ff0043',
     white: '#fff',
